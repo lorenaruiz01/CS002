de90e40310f9d2f95690f3cbc3e34d486e8fdf8c
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2026-09-10 15:36:28 -0700

    declaring dollars variable

--- A02-CS002.cpp
+++ A02-CS002.cpp
@@ -2,12 +2,13 @@
 
 int main()
 {
-    int saved_cents, total_cents, quarters, dimes, nickels, pennies;
+    int saved_cents, total_cents, dollars, quarters, dimes, nickels, pennies;
 
     std::cout << "Please enter a number greater than 500: ";
     std:: cin >> total_cents;
     saved_cents = total_cents; //preserving the original value so we can print at the end.
 
+    
     quarters = total_cents / 25; //figures out how many quarters
     total_cents = total_cents - (quarters * 25); //figures out how mny cents remain.
 
