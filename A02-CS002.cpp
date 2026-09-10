28bfc1d3845a4dfe8fc4c009d58929a865a87be5
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2026-09-10 15:39:40 -0700

    calculating dollars variable

--- A02-CS002.cpp
+++ A02-CS002.cpp
@@ -8,9 +8,11 @@ int main()
     std:: cin >> total_cents;
     saved_cents = total_cents; //preserving the original value so we can print at the end.
 
-    
+    dollars = total_cents / 100; //figures out how many dollars 
+    total_cents = total_cents - (dollars * 100); //figures out how many cents remain.
+
     quarters = total_cents / 25; //figures out how many quarters
-    total_cents = total_cents - (quarters * 25); //figures out how mny cents remain.
+    total_cents = total_cents - (quarters * 25); //figures out how many cents remain.
 
     dimes = total_cents / 10; //figures out how many dimes
     total_cents = total_cents - (dimes * 10); //figures out how many cents remain.
