ea5cb36ea041b8099e954bc16ac690289b2fea32
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2026-09-14 11:03:34 -0700

    while loop

--- A07-Functions-Divisible-by-8.cpp
+++ A07-Functions-Divisible-by-8.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int main() {
     
     // Get user input
-    
-    
+    cout << "Enter a number: ";
+    int number;
+    cin >> number;
+    while (number  <= 0) {
+        cout << "Please enter a positive integer: ";
+        cin >> number;
+    }
 }
