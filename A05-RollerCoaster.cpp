ff7fa5aab9e8d107fc5d3c6c36b5404c4c13b19b
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2026-09-14 10:37:12 -0700

    add else condition

--- A05-RollerCoaster.cpp
+++ A05-RollerCoaster.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 int main () {
     int weight, age;
-    bool hasPermission;
 
     // Get user input
     cout << "Enter your age: ";
@@ -13,27 +12,27 @@ int main () {
     cout << "Enter your weight (in pounds): ";
     cin >> weight;
 
-    // Check if user can ride the black roller coaster
+    // Check which roller coaster the user can ride
     if (age <= 10 && weight <= 80) {
         cout << "You can ride the black roller coaster." << std::endl;
-    } 
+    } else
     
     if (age <= 10 && weight >= 80 && weight <= 200) {
         cout << "You can ride the green roller coaster." << std::endl;
-    }
+    } else
 
     if (age <= 10 && weight >= 200) {
         cout << "You can ride the yellow roller coaster." << std::endl;
-    }
+    } else
 
     if (age <= 20 && age > 10 && weight <= 80) {
         cout << "You can ride the silver roller coaster." << std::endl;
-    }
+    } else
 
     if (age <= 20 && age > 10 && weight >= 80 && weight <= 200) {
         cout << "You can ride the red roller coaster." << std::endl;
-    }  
-      
+    } else
+
     if (age <= 20 && age > 10 && weight >= 200) {
         cout << "You can ride the purple roller coaster." << std::endl;
     } else {
