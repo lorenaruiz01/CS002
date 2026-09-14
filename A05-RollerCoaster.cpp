eecd8b5745c83dd9a0a964b21afdf1015cabae08
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2026-09-14 10:34:50 -0700

    change prints

--- A05-RollerCoaster.cpp
+++ A05-RollerCoaster.cpp
@@ -15,99 +15,31 @@ int main () {
 
     // Check if user can ride the black roller coaster
     if (age <= 10 && weight <= 80) {
-        hasPermission = true;
-    } else {
-        hasPermission = false;
-    }
-
-    // Display result
-    if (hasPermission) {
         cout << "You can ride the black roller coaster." << std::endl;
-    } else {
-        cout << "You are not allowed to ride the black roller coaster." << std::endl;
-    }
-
-    return 0;
-
-    // Check if user can ride the green roller coaster
+    } 
+    
     if (age <= 10 && weight >= 80 && weight <= 200) {
-        hasPermission = true;
-    } else {
-        hasPermission = false;
-    }
-
-    // Display result
-    if (hasPermission) {
         cout << "You can ride the green roller coaster." << std::endl;
-    } else {
-        cout << "You are not allowed to ride the green roller coaster." << std::endl;
     }
-
-    return 0;
 
     if (age <= 10 && weight >= 200) {
-        hasPermission = true;
-    } else {
-        hasPermission = false;
-    }
-
-    // Display result
-    if (hasPermission) {
         cout << "You can ride the yellow roller coaster." << std::endl;
-    } else {
-        cout << "You are not allowed to ride the yellow roller coaster." << std::endl;
     }
-
-    return 0;
 
     if (age <= 20 && age > 10 && weight <= 80) {
-        hasPermission = true;
-    } else {
-        hasPermission = false;
-    }
-
-    // Display result
-    if (hasPermission) {
         cout << "You can ride the silver roller coaster." << std::endl;
-    } else {
-        cout << "You are not allowed to ride the silver roller coaster." << std::endl;
     }
-
-    return 0;
 
     if (age <= 20 && age > 10 && weight >= 80 && weight <= 200) {
-        hasPermission = true;
-    } else {
-        hasPermission = false;
-    }
-
-    // Display result
-    if (hasPermission) {
         cout << "You can ride the red roller coaster." << std::endl;
-    } else {
-        cout << "You are not allowed to ride the red roller coaster." << std::endl;
-    }
-
-    return 0;
-
+    }  
+      
     if (age <= 20 && age > 10 && weight >= 200) {
-        hasPermission = true;
-    } else {
-        hasPermission = false;
-    }
-
-    // Display result
-    if (hasPermission) {
         cout << "You can ride the purple roller coaster." << std::endl;
     } else {
-        cout << "You are not allowed to ride the purple roller coaster." << std::endl;
-    }
-
-    return 0;
-
-    else {
         cout << "You can ride the pink roller coaster." << std::endl;
     }
 
     return 0;
-}
+
+ }
