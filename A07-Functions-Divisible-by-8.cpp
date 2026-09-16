3a0595ff39417787bf2eebcd84764cc692412f50
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2026-09-16 10:44:38 -0700

    comment corrections

--- A07-Functions-Divisible-by-8.cpp
+++ A07-Functions-Divisible-by-8.cpp
@@ -1,6 +1,6 @@
 // Name: Lorena Ruiz (A10191832)
 
-// program that gets a positive integer from the user and prints all even numbers from zero to that number. Then returns true for each number divisible by 8. 
+// Program that gets a positive integer from the user and prints all even numbers from zero to that number. Then returns true for each number divisible by 8. 
 
 #include <iostream>
 using namespace std;
@@ -26,19 +26,19 @@ int main() {
     return 0;
 }
 
-// prints all even numbers from zero to the number it was passed from main()
+// Prints all even numbers from zero to the number it was passed from main()
 void print_evens(int numberFromUser) {
     for (int i = 0; i <= numberFromUser; i += 2) {
         cout << i << "\n";
 
-        // function call to check if the number is divisible by 8
+        // Function call to check if the number is divisible by 8
         if (is_divisible_by_8(i)) {
             cout << i << " is divisible by 8. \n";
         }
     }
 } 
 
-// returns true for each number divisible by 8
+// Returns true for each number divisible by 8
 bool is_divisible_by_8(int numberFromUser) {
     return numberFromUser % 8 == 0;
     }
