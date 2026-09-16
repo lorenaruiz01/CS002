fc2e7742719735eeaf846a3dbb8dd857ecb1e41d
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2026-09-16 09:34:56 -0700

    comment correction

--- A09-Call-By-Value-Reference.cpp
+++ A09-Call-By-Value-Reference.cpp
@@ -1,6 +1,6 @@
 // Name: Lorena Ruiz (A10191832)
 
-// program that updates a student's exam score by adding extra credit point. 
+// program that updates a student's exam score by adding extra credit points. 
 // Implement two functions - one that uses call by value and another that uses call by reference - to see how they affect the original score.
 
 #include <iostream>
