98eb1e0f1e3fdb60718d7df89581d7681ca87466
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2026-09-16 10:42:38 -0700

    corrections to loops

--- A07-Functions-Divisible-by-8.cpp
+++ A07-Functions-Divisible-by-8.cpp
@@ -5,6 +5,9 @@
 #include <iostream>
 using namespace std;
 
+void print_evens(int numberFromUser);
+bool is_divisible_by_8(int numberFromUser);
+
 // Gets a positive integer from the user. 
 int main() {
     
@@ -20,18 +23,21 @@ int main() {
     // Call the function to print even numbers
     print_evens(numberFromUser);
 
+    return 0;
 }
+
 // prints all even numbers from zero to the number it was passed from main()
 void print_evens(int numberFromUser) {
     for (int i = 0; i <= numberFromUser; i += 2) {
-        cout << i << " ";
-    }
-    cout << endl;
-    
-    // returns true for each number divisible by 8
-    for (int i = 0; i <= numberFromUser; i++) {
-        if (i % 8 == 0) {
-            cout << i << " is divisible by 8." << endl;
+        cout << i << "\n";
+
+        if (is_divisible_by_8(i)) {
+            cout << i << " is divisible by 8. \n";
         }
     }
-}
+} 
+
+// returns true for each number divisible by 8
+bool is_divisible_by_8(int numberFromUser) {
+    return numberFromUser % 8 == 0;
+    }
