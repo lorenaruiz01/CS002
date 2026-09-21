bb9e1450d05497aee429294a09221cf312fa38ef
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2026-09-21 09:46:37 -0700

    start assignment

---
+++ A06-Switch-enum-Chinese-zodiac.cpp
@@ -0,0 +1,22 @@
+// Lorena Ruiz A10191832
+// A06-Switch-enum-Chinese-zodiac.cpp
+
+// formula for converting year to chinese zodiac animal
+// year - 4 % 12 = remainder of the year when divided by 12
+// match the remainder to the corresponding animal in the Chinese zodiac cycle
+
+/*
+Remainder | Animal
+0         | Rat
+1         | Ox
+2         | Tiger
+3         | Rabbit
+4         | Dragon
+5         | Snake
+6         | Horse
+7         | Goat
+8         | Monkey
+9         | Rooster
+10        | Dog
+11        | Pig
+*/
